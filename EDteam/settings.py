1b30d2e765158f990dbbfa02521d6951601ea3a6
JohnCard <johncard962@gmail.com>
2024-08-18 22:34:47 -0600

    ready

--- EDteam/settings.py
+++ EDteam/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv('KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-react.vercel.app', '*']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'EDteam.urls'
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR/'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -129,8 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', "static")
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
